87862b25f9ddd6f61f245bb779e02928f6ebe06c
Danila Fokin <sir.sotoros@yandex.ru>
2018-10-15 13:52:58 +0300

    Changed

---
+++ vp_lab_8/Main.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace vp_lab_8
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.textBoxDescription = new System.Windows.Forms.TextBox();
+            this.listView = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // textBoxDescription
+            // 
+            this.textBoxDescription.Location = new System.Drawing.Point(12, 8);
+            this.textBoxDescription.Multiline = true;
+            this.textBoxDescription.Name = "textBoxDescription";
+            this.textBoxDescription.ReadOnly = true;
+            this.textBoxDescription.Size = new System.Drawing.Size(1174, 41);
+            this.textBoxDescription.TabIndex = 2;
+            // 
+            // listView
+            // 
+            this.listView.Activation = System.Windows.Forms.ItemActivation.OneClick;
+            this.listView.AllowColumnReorder = true;
+            this.listView.FullRowSelect = true;
+            this.listView.HoverSelection = true;
+            this.listView.Location = new System.Drawing.Point(12, 55);
+            this.listView.Name = "listView";
+            this.listView.ShowItemToolTips = true;
+            this.listView.Size = new System.Drawing.Size(1174, 538);
+            this.listView.TabIndex = 1;
+            this.listView.UseCompatibleStateImageBehavior = false;
+            this.listView.View = System.Windows.Forms.View.Details;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(1198, 605);
+            this.Controls.Add(this.textBoxDescription);
+            this.Controls.Add(this.listView);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Main";
+            this.Text = "Yandex.Новости: В мире";
+            this.Load += new System.EventHandler(this.Main_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBoxDescription;
+        private System.Windows.Forms.ListView listView;
+    }
+}
+
---
+++ vp_lab_8/Main.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace vp_lab_8
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+
+            // Загрузка ленты
+            XElement rootTag = XElement.Load("https://news.yandex.ru/world.rss").Element(XName.Get("channel")); ;
+
+            // Создание корневого элемента
+            TreeNode rootNode = new TreeNode(rootTag.Name.ToString());
+
+            // Поиск дочерних элементов корневого элемента
+            FindChildrenTags(rootNode, rootTag);
+
+            //// Добавление корневого элемента в дерево
+            //treeView.Nodes.Add(rootNode);
+
+            // Формируем заголовок окна
+            Text = rootTag.Element(XName.Get("title")).Value + " " + rootTag.Element(XName.Get("lastBuildDate")).Value;
+
+            // Формирование описания
+            textBoxDescription.Text = rootTag.Element(XName.Get("description")).Value;
+
+            // Добавляем название столбцов
+            listView.Items.Clear();
+            listView.Columns.Clear();
+
+            listView.Columns.Add("Заголовок");
+            listView.Columns.Add("Описание");
+            listView.Columns.Add("Дата публикации");
+            listView.Columns.Add("Ссылка");
+
+            // Формируем таблицу новостей
+            foreach (XElement element in rootTag.Elements())
+            {
+                if (element.Name.ToString().ToLower() == "item")
+                {
+                    // Заполнение таблицы
+                    XElement title = element.Element(XName.Get("title"));
+                    ListViewItem item = new ListViewItem(title.Value);
+
+                    XElement description = element.Element(XName.Get("description"));
+                    item.SubItems.Add(description.Value);
+
+                    XElement date = element.Element(XName.Get("pubDate"));
+                    item.SubItems.Add(date.Value);
+
+                    XElement link = element.Element(XName.Get("link"));
+                    item.SubItems.Add(link.Value);
+
+                    listView.Items.Add(item);
+                }
+            }
+
+            // Изменение размеров колонок списка
+            listView.AutoResizeColumns(ColumnHeaderAutoResizeStyle.ColumnContent);
+            listView.AutoResizeColumns(ColumnHeaderAutoResizeStyle.HeaderSize);
+        }
+
+        /// <summary>
+        /// Поиск и добавление дочерних элементов
+        /// </summary>
+        /// <param name="node">Корневой узел</param>
+        /// <param name="tag">Корневой тег</param>
+        private void FindChildrenTags(TreeNode node, XElement tag)
+        {
+            // Связь узла тега
+            node.Tag = tag;
+
+            // Получение дочерних элементов
+            IEnumerable<XElement> childTags = tag.Elements();
+
+            if (childTags != null)
+            {
+                foreach (XElement childTag in childTags)
+                {
+                    // Создание дочернего узла
+                    TreeNode childNode = new TreeNode(childTag.Name.ToString());
+
+                    // Добавление поддочерних элементов
+                    FindChildrenTags(childNode, childTag);
+
+                    // Добавление дочернего узла в дерево
+                    node.Nodes.Add(childNode);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Событие вызываемое при ЛКМ по узлу дерева
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void treeView_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            // Получаем объект связанный с узлом
+            object tag = e.Node.Tag;
+
+            // Проверяем на пренадлежность к классу XElement
+            if (tag is XElement)
+            {
+                // Приведение типа
+                XElement element = (XElement)tag;
+             
+                if (element.Name.ToString().ToLower() == "item")
+                {
+                    // Добавляем название столбцов
+                    listView.Items.Clear();
+                    listView.Columns.Clear();
+
+                    listView.Columns.Add("Заголовок");
+                    listView.Columns.Add("Описание");
+                    listView.Columns.Add("Дата публикации");
+                    listView.Columns.Add("Ссылка");
+
+                    // Заполнение таблицы
+                    XElement title = element.Element(XName.Get("title"));
+                    ListViewItem item = new ListViewItem(title.Value);
+
+                    XElement description = element.Element(XName.Get("description"));
+                    item.SubItems.Add(description.Value);
+
+                    XElement date = element.Element(XName.Get("pubDate"));
+                    item.SubItems.Add(date.Value);
+
+                    XElement link = element.Element(XName.Get("link"));
+                    item.SubItems.Add(link.Value);
+
+                    listView.Items.Add(item);
+                }
+            }
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
